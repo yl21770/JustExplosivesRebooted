b6e46cccf3acbb17f7a02eed42623daad35ca162
jd-x <jdeaxx@gmail.com>
2016-09-22 00:13:35 +0800

    First commit

---
+++ JustExplosivesRebooted/scripts/jxr_timebomb.lua
@@ -0,0 +1,121 @@
+function init()
+  if storage.state == nil then
+    storage.state = false
+  end
+
+  self.interactive = config.getParameter("interactive", true)
+  object.setInteractive(self.interactive)
+
+  if config.getParameter("inputNodes") then
+    processWireInput()
+  end
+
+  self.start = false
+  self.timer = config.getParameter("timeToExplode")
+
+  toggleActivation(storage.state)  
+end
+
+function onNodeConnectionChange(args)
+--Receives input when connected to other wire objects
+  processWireInput()
+end
+
+function onInputNodeChange(args)
+--Receives input from other wire objects
+  processWireInput()
+end
+
+function onInteraction(args)
+--Manual activation
+  if not config.getParameter("inputNodes") or not object.isInputNodeConnected(0) then
+    storage.state = not storage.state
+    --sb.logInfo("Inbound signal detected (Manual)! Bomb activated.")
+    toggleActivation(storage.state)
+  end
+end
+
+function toggleActivation(state)
+--Enables bomb state toggling (ON or OFF)
+  --object.setAllOutputNodes(state)
+  if state then
+    animator.setAnimationState("switchState", "on")
+    self.start = true
+  else
+    animator.setAnimationState("switchState", "off")
+    self.start = false
+    self.timer = config.getParameter("timeToExplode")
+  end
+end
+
+function processWireInput()
+--Enables bomb state toggling via wire (makes manual activation disabled)
+  if object.isInputNodeConnected(0) then
+    object.setInteractive(false)
+    storage.state = object.getInputNodeLevel(0)
+    toggleActivation(storage.state)
+    --sb.logInfo("Inbound signal detected (Wire)! Bomb activated.")
+  elseif self.interactive then
+    object.setInteractive(true)
+  end
+end
+
+function update(dt)
+  --Check if countdown has been started
+  if self.start then
+    --If bomb is activated
+    if storage.state then
+      --Start timer (default is 6 seconds, modify in .object file)
+      if self.timer > 0 then
+        --Trigger explosion
+        if self.timer <= 1 then
+          --Check if projectile specified in .object file is present
+          if config.getParameter("firstProjectile") ~= nil then
+            world.spawnProjectile(config.getParameter("firstProjectile"), object.toAbsolutePosition({ 0.2, 1 }), entity.id())
+          end
+          --Destroy object
+          object.smash()
+        end
+      end
+    --Update Timer
+    self.timer = math.max(0, self.timer - dt)
+    end
+  end
+end
+
+function die()
+--Triggers other nearby explosives upon explosion
+  if config.getParameter("firstProjectile") ~= nil then
+    world.spawnProjectile(config.getParameter("firstProjectile"), object.toAbsolutePosition({ 0.2, 1 }), entity.id())
+  end
+end
+
+--[[function init(args)
+  Use the code below for proximity bombs or anything that requires proximity detection
+  local detectArea = config.getParameter("detectArea")
+  local pos = entity.position()
+  self.detectArea = {
+    {pos[1] + detectArea[1][1], pos[2] + detectArea[1][2]},
+    {pos[1] + detectArea[2][1], pos[2] + detectArea[2][2]}
+  }
+end
+
+--[[function update(dt)
+  if self.triggerTimer > 0 then
+    self.triggerTimer = math.max(0, self.triggerTimer - dt)
+    if self.triggerTimer == 0 then
+      --if config.getParameter("firstProjectile") ~= nil then
+        world.spawnProjectile("timebombexplosion",
+        entity.toAbsolutePosition({0.2, 1}), entity.id(), {0, 5}, false,{})
+      --end
+      object.setAllOutputNodes(true)
+      object.smash()
+    end
+  else
+    local radius = config.getParameter("detectArea")
+    local entityIds = world.entityQuery(self.detectArea[1],
+    self.detectArea[2], { includedTypes = { "creature" }, boundMode = "CollisionArea"})
+    if #entityIds > 0 then
+      trigger()
+    end
+  end]]
