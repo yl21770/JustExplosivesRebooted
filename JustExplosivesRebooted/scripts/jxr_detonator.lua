5548910bfaab54ce1491775b106dce24b1050e1c
jd-x <jdeaxx@gmail.com>
2016-10-11 15:32:33 +0800

    Fix detonator not sending signal to wired objects

--- JustExplosivesRebooted/scripts/jxr_detonator.lua
+++ JustExplosivesRebooted/scripts/jxr_detonator.lua
@@ -4,20 +4,20 @@ function init()
     storage.state = false
   end
 
-  self.detectEntityTypes = config.getParameter("detectEntityTypes")
   self.pos = entity.position()
   self.pos = {self.pos[1] + 0.5, self.pos[2] + 0.5}
   self.detectRange = config.getParameter("detectRange")
 
-  object.setInteractive(config.getParameter("interactive" or true))
-  object.setAllOutputNodes(false)
-  toggleActivation(storage.state)
   self.timer = 0
+
+  --object.setAllOutputNodes(false)
+  toggleActivation(storage.state)
+  object.setInteractive(config.getParameter("interactive" or true))
 end
 
 function onInteraction()
 --Manual activation
-  if storage.state == false then
+  if not storage.state then
     storage.state = true
   end
   toggleActivation(storage.state)
@@ -32,12 +32,13 @@ end
 
 function entitiesInRange()
 --Check if players are nearby
-  local count = 0
-
   local playerIDs = world.playerQuery(self.pos, self.detectRange)
-  count = count + #playerIDs
 
-  return count > 0
+  if #playerIDs > 0 then
+    return true
+  end
+
+  return false
 end
 
 function update(dt)
@@ -46,14 +47,13 @@ function update(dt)
     if self.timer > 0 then
       self.timer = self.timer - dt
     else
-      local playersFound = entitiesInRange()
+      local found = entitiesInRange()
       --Detonate only when there are no nearby players
-      if not playersFound then
+      if not found then
         object.setAllOutputNodes(true)
-        sb.logInfo("JXR::Output node status: " .. tostring(object.getOutputNodeLevel(0)))
         die()
       end
-      self.timer = 0.4
+      self.timer = 0.2
     end
   end
 end
