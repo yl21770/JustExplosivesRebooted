f5db41b46f524744a0018699e7cc072ac3e59a39
jd-x <jdeaxx@gmail.com>
2016-10-18 16:54:09 +0800

    Add tar barrel
    * Removed unnecessary lines in liquid barrel explosion

--- JustExplosivesRebooted/scripts/jxr_liquidbarrel.lua
+++ JustExplosivesRebooted/scripts/jxr_liquidbarrel.lua
@@ -6,8 +6,8 @@ function die()
   liquidType = config.getParameter("liquidType")
   
   if liquidType ~= nil and config.getParameter("firstProjectile") ~= nil then
-    world.spawnProjectile(config.getParameter("firstProjectile"), object.toAbsolutePosition({0.2, 1}), entity.id())
-    world.spawnLiquid(self.pos, liquidType, 20)
+    world.spawnProjectile(config.getParameter("firstProjectile"), object.toAbsolutePosition({0, 0}), entity.id())
+    world.spawnLiquid(self.pos, liquidType, 25)
     object.smash()
   end
 end
