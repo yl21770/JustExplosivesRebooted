c9d7695d786ecafbb20a821ffd88754e330274c5
jd-x <jdeaxx@gmail.com>
2016-10-19 02:02:59 +0800

    Add tentacle barrel (spawns liquid slime for now)
    * Lowered harvestLevel value
    * Decreased spawned liquid for barrels
    * Removed unnecessary code for some explosives

--- JustExplosivesRebooted/scripts/jxr_liquidbarrel.lua
+++ JustExplosivesRebooted/scripts/jxr_liquidbarrel.lua
@@ -7,7 +7,7 @@ function die()
   
   if liquidType ~= nil and config.getParameter("firstProjectile") ~= nil then
     world.spawnProjectile(config.getParameter("firstProjectile"), object.toAbsolutePosition({0, 0}), entity.id())
-    world.spawnLiquid(self.pos, liquidType, 25)
+    world.spawnLiquid(self.pos, liquidType, 15)
     object.smash()
   end
 end
